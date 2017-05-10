85dfcc5845065e99241d7fa34a3df95118987b8d
ColmeSH <mauriziocontatto@gmail.com>
2017-05-10 14:24:37 +0200

    update class bookmarks

---
+++ mysite/bookmarks/class/bookmarks.py
@@ -0,0 +1,11 @@
+class Bookmarks(object):
+
+    def __init__(self, student_owner):
+        self.student_owner = student_owner
+        self.list_marks = []
+
+    def __str__(self):
+        return "OWNER: {}".format(self.student_owner)
+
+    def __repr__(self):
+        return self.__str__()
